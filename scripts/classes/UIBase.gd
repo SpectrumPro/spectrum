3e571c77f70bcac98b697ff9cc6bdd40966f2a0f
Liam <git@liamsherwin.com>
2026-01-19 22:11:30 +1300

    Added support for queuing callables

--- scripts/classes/UIBase.gd
+++ scripts/classes/UIBase.gd
@@ -22,6 +22,18 @@ var _self_class_name: String = "UIBase"
 ## The name of this UIBase
 var _ui_name: String = _self_class_name
 
+## Stores all queued callables for this frame
+var _queue_order: Array[Callable]
+
+## Stores arguments for each queued callable
+var _queue_args: Dictionary[Callable, Array]
+
+## True if _call_queue has already been call_defered()
+var _queue_call_defered: bool = false
+
+## True if _call_queue is running currently
+var _queue_is_calling: bool = false
+
 ## Settings for this component
 var settings_manager: SettingsManager = SettingsManager.new()
 
@@ -42,6 +54,29 @@ func _init() -> void:
 ## Gets the uuid
 func uuid() -> String:
 	return _uuid
+
+
+## Queues a callable for execution, will only all it to be called once per frame. Uses call_defered
+func queue(p_callable: Callable, p_args: Array[Variant] = []) -> bool:
+	if _queue_is_calling:
+		p_callable.callv(p_args)
+		
+		return true
+		
+	elif _queue_args.has(p_callable):
+		_queue_args[p_callable] = p_args
+		
+		return false
+		
+	else:
+		_queue_order.append(p_callable)
+		_queue_args[p_callable] = p_args
+		
+		if not _queue_call_defered:
+			_call_queue.call_deferred()
+			_queue_call_defered = true
+		
+		return true
 
 
 ## Sets the UI name
@@ -69,6 +104,19 @@ func get_class_tree() -> Array[String]:
 func _set_class_name(p_class_name: String) -> void:
 	_self_class_name = p_class_name
 	_class_tree.append(p_class_name)
+
+
+## Calls all the Callables in the queue
+func _call_queue() -> void:
+	_queue_is_calling = true
+	
+	for callable: Callable in _queue_order:
+		callable.callv(_queue_args[callable])
+	
+	_queue_order.clear()
+	_queue_args.clear()
+	_queue_is_calling = false
+	_queue_call_defered = false
 
 
 ## Serialize this ClientComponent into a Dictionary
