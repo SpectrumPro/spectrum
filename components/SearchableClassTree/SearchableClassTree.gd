6fbc950a434b982a1777cd6da1607e0552a63d49
Liam <git@liamsherwin.com>
2026-01-27 13:48:42 +1300

    Update copyright notice

--- components/SearchableClassTree/SearchableClassTree.gd
+++ components/SearchableClassTree/SearchableClassTree.gd
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Liam Sherwin. All rights reserved.
+# Copyright (c) 2026 Liam Sherwin. All rights reserved.
 # This file is part of the Spectrum Lighting Controller, licensed under the GPL v3.0 or later.
 # See the LICENSE file for details.
 
