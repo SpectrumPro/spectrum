d916a1cd0466e57a8fcd3ba2f415bbc7debc3a3f
Liam <git@liamsherwin.com>
2026-03-30 01:01:14 +1300

    Fixed open command palette shortcut

--- InputConfig.gd
+++ InputConfig.gd
@@ -2,7 +2,7 @@
 static var internal_actions: Dictionary[String, Callable] = {
 	"clear_programmer": Programmer.clear,
 	"ui_cancel": Interface.hide_all_popup_panels,
-	"command_palette": Interface.toggle_popup_visable.bind(UICommandPalette, self),
+	"command_palette": handle_command_pallete_action,
 	"screenshot": Interface.take_screenshot,
 }
 
@@ -10,3 +10,8 @@ static var internal_actions: Dictionary[String, Callable] = {
 static var config: Dictionary[String, Variant] = {
 	"internal_actions": internal_actions
 }
+
+
+## Handles the open command palette action
+static func handle_command_pallete_action() -> void:
+	Interface.toggle_popup_visable(UICommandPalette, InputServer)
